efdf2b41096782fe53006797939bd2a8e5f1df60
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-25 18:24:22 +0400

    laying out widgets: spinBox - slider

--- QtGuiApp/QtGuiApp/main.cpp
+++ QtGuiApp/QtGuiApp/main.cpp
@@ -1,12 +1,33 @@
 #include <QtWidgets\qapplication.h>
-#include <QtWidgets\qpushbutton.h>
+#include <QtWidgets\qboxlayout.h>
+#include <QtWidgets\qspinbox.h>
+#include <QtWidgets\qslider.h>
 
 
 int main(int argc, char** argv) {
+    
     QApplication app(argc, argv);
-    QPushButton *button = new QPushButton("Quit");
-    QObject::connect(button, SIGNAL(clicked()),
-                     &app, SLOT(quit()));
-    button->show();
+    
+    QWidget *window = new QWidget;
+    window->setWindowTitle("Enter your age");
+
+    QSpinBox *spinBox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinBox->setRange(0, 130);
+    slider->setRange(0, 130);
+
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                     slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)),
+                     spinBox, SLOT(setValue(int)));
+    spinBox->setValue(20);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    window->setLayout(layout);
+
+    window->show();
+
     return app.exec();
 }
