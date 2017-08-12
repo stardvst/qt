58c8847ac08d0e3306c73ff42c37578e855901aa
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-08-13 00:05:07 +0400

    centered buttons

--- QtGuiApp/QtGuiApp/main.cpp
+++ QtGuiApp/QtGuiApp/main.cpp
@@ -1,12 +1,35 @@
 #include <qapplication.h>
-
-#include "window.h"
+#include <qpushbutton.h>
+#include <qlayout.h>
 
 int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
 
-    Window window;
+    QWidget window;
+    window.setFixedSize(200, 100);
+
+    QPushButton *buttonInfo = new QPushButton("Info", &window);
+    //buttonInfo->setGeometry(10, 10, 80, 30);
+    buttonInfo->setMaximumWidth(80);
+
+    QPushButton *buttonQuit = new QPushButton("Quit", &window);
+    //buttonQuit->setGeometry(10, 10, 110, 30);
+    buttonQuit->setMinimumWidth(110);
+
+    QObject::connect(buttonInfo, SIGNAL(clicked()), qApp, SLOT(aboutQt()));
+    QObject::connect(buttonQuit, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    QHBoxLayout *hlayout = new QHBoxLayout;
+    QVBoxLayout *vlayout = new QVBoxLayout;
+
+    hlayout->addWidget(buttonInfo, 0, Qt::AlignHCenter);
+
+    vlayout->addLayout(hlayout);
+    vlayout->addWidget(buttonQuit);
+    vlayout->setAlignment(Qt::AlignCenter);
+
+    window.setLayout(vlayout);
     window.show();
 
     return app.exec();
