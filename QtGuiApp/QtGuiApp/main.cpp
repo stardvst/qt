44ad9423d74ed0c58c95dc62a1d2cfb4f72f1b94
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-08-13 19:24:56 +0400

    model/view: 1 model, 3 views

--- QtGuiApp/QtGuiApp/main.cpp
+++ QtGuiApp/QtGuiApp/main.cpp
@@ -1,21 +1,41 @@
+#include <qfilesystemmodel.h>
 #include <qapplication.h>
-#include <qtextedit.h>
+#include <qmainwindow.h>
+#include <qtableview.h>
+#include <qtreeview.h>
+#include <qlistview.h>
 #include <qsplitter.h>
 
 int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
 
-    QTextEdit *editor1 = new QTextEdit;
-    QTextEdit *editor2 = new QTextEdit;
-    QTextEdit *editor3 = new QTextEdit;
+    QFileSystemModel *model = new QFileSystemModel;
+    model->setRootPath(QDir::currentPath());
 
-    QSplitter splitter(Qt::Horizontal);
-    splitter.addWidget(editor1);
-    splitter.addWidget(editor2);
-    splitter.addWidget(editor3);
+    QSplitter *topSplitter = new QSplitter(Qt::Horizontal);
 
-    splitter.show();
+    QTreeView *tree = new QTreeView(topSplitter);
+    tree->setModel(model);
+    tree->setRootIndex(model->index(QDir::currentPath()));
 
+    QListView *list = new QListView(topSplitter);
+    list->setModel(model);
+    list->setRootIndex(model->index(QDir::currentPath()));
+
+    QTableView *table = new QTableView;
+    table->setModel(model);
+    table->setRootIndex(model->index(QDir::currentPath()));
+
+    QSplitter *mainSplitter = new QSplitter(Qt::Vertical);
+    mainSplitter->addWidget(topSplitter);
+    mainSplitter->addWidget(table);
+    mainSplitter->setStretchFactor(1, 1); // i want to give table more space ;)
+     
+    QMainWindow window;
+    window.setWindowTitle("Two views onto the same file system model");
+    window.setCentralWidget(mainSplitter);
+    window.show();
+    
     return app.exec();
 }
