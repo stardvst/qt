1c91cc17ea042c187c325ab9ce143bd8450f5681
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-08-13 11:49:22 +0400

    horizontal splitter example

--- QtGuiApp/QtGuiApp/main.cpp
+++ QtGuiApp/QtGuiApp/main.cpp
@@ -1,36 +1,21 @@
 #include <qapplication.h>
-#include <qpushbutton.h>
-#include <qlayout.h>
+#include <qtextedit.h>
+#include <qsplitter.h>
 
 int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
 
-    QWidget window;
-    window.setFixedSize(200, 100);
+    QTextEdit *editor1 = new QTextEdit;
+    QTextEdit *editor2 = new QTextEdit;
+    QTextEdit *editor3 = new QTextEdit;
 
-    QPushButton *buttonInfo = new QPushButton("Info", &window);
-    //buttonInfo->setGeometry(10, 10, 80, 30);
-    buttonInfo->setMaximumWidth(80);
+    QSplitter splitter(Qt::Horizontal);
+    splitter.addWidget(editor1);
+    splitter.addWidget(editor2);
+    splitter.addWidget(editor3);
 
-    QPushButton *buttonQuit = new QPushButton("Quit", &window);
-    //buttonQuit->setGeometry(10, 10, 110, 30);
-    buttonQuit->setMinimumWidth(110);
-
-    QObject::connect(buttonInfo, SIGNAL(clicked()), qApp, SLOT(aboutQt()));
-    QObject::connect(buttonQuit, SIGNAL(clicked()), qApp, SLOT(quit()));
-
-    QHBoxLayout *hlayout = new QHBoxLayout;
-    QVBoxLayout *vlayout = new QVBoxLayout;
-
-    hlayout->addWidget(buttonInfo, 0, Qt::AlignHCenter);
-
-    vlayout->addLayout(hlayout);
-    vlayout->addWidget(buttonQuit);
-    vlayout->setAlignment(Qt::AlignCenter);
-
-    window.setLayout(vlayout);
-    window.show();
+    splitter.show();
 
     return app.exec();
 }
