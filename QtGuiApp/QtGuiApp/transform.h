c6fb61b51e9f0fe40dd874f15f5e401de09fb0fc
Lilia <lilia.manukyan@hotmail.com>
2019-12-22 12:02:39 +0400

    combining transforms

--- QtGuiApp/QtGuiApp/transform.h
+++ QtGuiApp/QtGuiApp/transform.h
@@ -13,10 +13,22 @@ protected:
 		painter.setPen(QPen(Qt::blue, 1, Qt::DashLine));
 		painter.drawRect(0, 0, 100, 100);
 
-		QMatrix matrix;
-		matrix.translate(50, 50);
-		matrix.rotate(45);
-		matrix.scale(0.5, 1.0);
+		//QMatrix matrix;
+		//matrix.translate(50, 50);
+		//matrix.rotate(45);
+		//matrix.scale(0.5, 1.0);
+
+		double pi = 3.14;
+		double a = pi / 180 * 45.0;
+		double sina = sin(a);
+		double cosa = cos(a);
+
+		QMatrix transMatrix(1, 0, 0, 1, 50.0, 50.0);
+		QMatrix rotMatrix(cosa, sina, -sina, cosa, 0, 0);
+		QMatrix scaleMatrix(0.5, 0, 0, 1.0, 0, 0);
+
+		// order is important
+		QMatrix matrix = scaleMatrix * rotMatrix * transMatrix;
 		painter.setMatrix(matrix);
 
 		painter.setFont(QFont("Helvetica", 24));
